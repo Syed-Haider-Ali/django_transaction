69e0abf2b308a6f309e3bb8a476fa7afcdd1d6d1
Syed Haider Ali <s.haider0303@gmail.com>
2023-10-02 11:31:40 +0500

    Initialized MVC pattern

---
+++ crud/controller.py
@@ -0,0 +1,15 @@
+
+
+class ProductController:
+
+    def create(self, request):
+        pass
+
+    def fetch(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .controller import ProductController
 
-# Create your views here.
+
+product_controller = ProductController()
+
+class ProductView(ModelViewSet):
+
+    def create(self, request):
+        return product_controller.create(request)
+
+    def fetch(self, request):
+        return product_controller.fetch(request)
+
+    def update(self, request):
+        return product_controller.update(request)
+
+    def destroy(self, request):
+        return product_controller.destroy(request)
